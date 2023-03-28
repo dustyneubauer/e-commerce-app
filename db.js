803aa92d1a7199e521e660459ad672a5175b3210
Dusty <dustyneubauer@gmail.com>
2023-03-28 11:03:33 -0500

    Added connection to server

---
+++ db.js
@@ -0,0 +1,8 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'dustyneubauer',
+  host: 'localhost',
+  database: 'acommerce',
+  password: '#Lilokilo2324',
+  port: 5432,
+})
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const app = express;
+const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  });
 
 app.listen(port, () =>{
     console.log(`App listening on port ${port}`)
